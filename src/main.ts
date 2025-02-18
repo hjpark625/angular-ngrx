3135de76c4db1e97723027cf2b57453a193ed820
hjpark625 <zero950@gmail.com>
2025-02-18 15:21:11 +0900

    test4

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,4 @@ import { bootstrapApplication } from '@angular/platform-browser'
 import { appConfig } from '@app/app.config'
 import { AppComponent } from '@app/app.component'
 
-console.log('test2')
-
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err))
